2d5a66e305c6f5f42c47fa55227003f472596aec
rspongnon <157622123+rspongnon@users.noreply.github.com>
2024-01-26 09:04:57 -0500

    Add files via upload

--- soilAPP.R
+++ soilAPP.R
@@ -5,6 +5,68 @@ library(readr)
 library(shiny)
 library(RCurl)
 
+
+cplots <- function(site) {
+  dataset <- FCE_sums %>% 
+    filter(SITENAME == site)
+  ggplot(data = FCE_sums[FCE_sums$SITENAME == site,],
+         mapping = aes(Year,per_C,color = per_C))+
+    geom_pointrange(aes(ymin =(per_C - error_C/2), ymax = (per_C +error_C/2)))+
+    ylim(0,55)}
+
+cplots("SRS3")
+
+
+nplots <- function(site) {
+  dataset <- FCE_sums %>% 
+    filter(SITENAME == site)
+  ggplot(data = FCE_sums[FCE_sums$SITENAME == site,],
+         mapping = aes(Year,per_N,color = per_N))+
+    geom_pointrange(aes(ymin =(per_N - error_N/2), ymax = (per_N +error_N/2)))+
+    ylim(0,5)}
+
+nplots("SRS3")
+
+pplots <- function(site) {
+  dataset <- FCE_sums %>% 
+    filter(SITENAME == site)
+  ggplot(data = dataset,
+         mapping = aes(Year,per_P,color = per_P))+
+    geom_pointrange(aes(ymin =(per_P - error_P/2), ymax = (per_P +error_P/2)))+
+    ylim(0,.15)}
+
+
+
+hplots <- function(site) {
+  dataset <- FCE_sums %>% 
+    filter(SITENAME == site)
+  ggplot(data = dataset,
+         mapping = aes(Year,per_H,color = per_H))+
+    geom_pointrange(aes(ymin =(per_H - error_H/2), ymax = (per_H +error_H/2)))+
+    ylim(0,5)}
+
+
+
+ratplots <- function(site) {
+  dataset <- FCE_sums %>% 
+    filter(SITENAME == site)
+  ggplot(data = dataset,
+         mapping = aes(Year,np_ratio,color = np_ratio))+
+    geom_point()+
+    ylim(0,250)}
+
+
+
+oplots <- function(site) {
+  dataset <- FCE_sums %>% 
+    filter(SITENAME == site)
+  ggplot(data = dataset,
+         mapping = aes(Year,per_O,color = per_O))+
+    geom_pointrange(aes(ymin =(per_O - error_O/2), ymax = (per_O +error_O/2)))+
+    ylim(0,100)}
+
+
+
 ui <- fluidPage(
   titlePanel("Florida Coastal Everglades soil data"),
   sidebarLayout(
